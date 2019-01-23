367d14ad57a85102b75ce2f75c1b8a2185a4f158
Max Powell <max_f_powell@hotmail.co.uk>
2019-01-23 16:52:51 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -20,7 +20,7 @@ function pressIt() {
 
 function submitIt() {
   $('form').on('submit', function() {
-    alert("You're form is going to be submitted now.")
+    alert("Your form is going to be submitted now.")
   })
 }
 
